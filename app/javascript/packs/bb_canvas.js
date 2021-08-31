680d5818060689db8d6ffb44f66ed8bfdce238d5
Ethan <eyeck3@gmail.com>
2021-08-31 13:10:37 -0700

    Got a canvas working for the basketball game part 2

--- app/javascript/packs/bb_canvas.js
+++ app/javascript/packs/bb_canvas.js
@@ -2,5 +2,8 @@ let canvas = document.getElementById('bb-game-canvas');
 
 const ctx = canvas.getContext('2d');
 
+
+ctx.clearRect(0, 0, 1200, 675);
+
 ctx.fillStyle = '#000';
-ctx.fillRect(20, 20, 200, 200);
+ctx.fillRect(20, 20, 100, 100);
