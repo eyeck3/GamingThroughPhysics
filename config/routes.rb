50017c1cb11e2f52f0c9d11b6460148f15565c72
Ethan <eyeck3@gmail.com>
2021-09-10 11:36:08 -0700

    Navbar recognizes current user, started the code for editing your password

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  get 'password', to: 'passwords#edit', as: :edit_password
+  patch 'password', to: 'passwords#update'
 end
